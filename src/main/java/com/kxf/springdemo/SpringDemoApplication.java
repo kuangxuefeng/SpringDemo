f02ee3cde924fd5e1cfc4123be7c75e917b346ed
kuangxuefeng <1024883177@qq.com>
2021-04-23 15:32:51 +0800

    SpringBoot整合Swagger-ui

--- src/main/java/com/kxf/springdemo/SpringDemoApplication.java
+++ src/main/java/com/kxf/springdemo/SpringDemoApplication.java
@@ -3,7 +3,10 @@ package com.kxf.springdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kxf/springdemo/controller/TestController.java
+++ src/main/java/com/kxf/springdemo/controller/TestController.java
@@ -2,14 +2,20 @@ package com.kxf.springdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kxf.springdemo.entity.SpringTableBean;
 import com.kxf.springdemo.service.SpringTableService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/springTable")
+@Api(tags = "springTable读写")
 public class TestController {
 	@Autowired
     private SpringTableService springTableService;
@@ -19,8 +25,9 @@ public class TestController {
 	 * @param id
 	 * @return
 	 */
-    @RequestMapping("/selectNameById")
-    public String selectNameById(@RequestParam(value="id", defaultValue="1") int id) {
+	@ApiOperation(value = "查询名称", notes = "查询名称")
+    @RequestMapping(value = "/selectNameById", method=RequestMethod.GET)
+    public String selectNameById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
     	SpringTableBean sb = springTableService.select(id);
         return sb.getName();
     }
@@ -28,8 +35,9 @@ public class TestController {
     /**
 	 * 查询对象   http://localhost:8080/springTable/selectById?id=2
 	 */
-    @RequestMapping("/selectById")
-    public SpringTableBean selectById(@RequestParam(value="id", defaultValue="1") int id) {
+	@ApiOperation(value = "查询对象", notes = "查询对象")
+	@RequestMapping(value = "/selectById", method=RequestMethod.GET)
+    public SpringTableBean selectById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
     	SpringTableBean sb = springTableService.select(id);
         return sb;
     }
@@ -37,8 +45,9 @@ public class TestController {
     /**
 	 * 增加对象   http://localhost:8080/springTable/addUser?name=tom
 	 */
-    @RequestMapping("/addUser")
-    public String addUser(@RequestParam(value="name", defaultValue="") String name, @RequestParam(value="info", defaultValue="") String info) {
+	@ApiOperation(value = "增加对象", notes = "增加对象")
+	@RequestMapping(value = "/addUser", method=RequestMethod.GET)
+    public String addUser(@ApiParam(value="用户名称", defaultValue="") @RequestParam(value="name", defaultValue="") String name, @ApiParam(value="用户备注", defaultValue="") @RequestParam(value="info", defaultValue="") String info) {
     	String re = "fail";
     	if (name!=null && name.length()>0) {
     		SpringTableBean sBean = new SpringTableBean();
