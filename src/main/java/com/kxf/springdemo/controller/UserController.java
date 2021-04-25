3c15f631d779d0b0ccb7a6cab73df4437cbc4107
kuangxuefeng <1024883177@qq.com>
2021-04-25 15:53:47 +0800

    返回信息封装

--- src/main/java/com/kxf/springdemo/controller/UserController.java
+++ src/main/java/com/kxf/springdemo/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kxf.springdemo.entity.UserBean;
+import com.kxf.springdemo.result.Result;
 import com.kxf.springdemo.service.UserService;
 
 import io.swagger.annotations.Api;
@@ -20,7 +21,7 @@ import io.swagger.annotations.ApiParam;
 @Api(tags = {"user操作"}, description = "/user")
 public class UserController {
 	@Autowired
-    private UserService springTableService;
+    private UserService userService;
 	
 	Logger logger=LoggerFactory.getLogger(UserController.class);
 
@@ -31,9 +32,9 @@ public class UserController {
 	 */
 	@ApiOperation(value = "查询名称", notes = "查询名称")
     @RequestMapping(value = "/selectNameById", method=RequestMethod.GET)
-    public String selectNameById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
-		UserBean sb = springTableService.select(id);
-        return sb.getName();
+    public Result<String> selectNameById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
+		UserBean sb = userService.select(id);
+        return Result.success(sb.getName());
     }
     
     /**
@@ -41,9 +42,9 @@ public class UserController {
 	 */
 	@ApiOperation(value = "查询对象", notes = "查询对象")
 	@RequestMapping(value = "/selectById", method=RequestMethod.GET)
-    public UserBean selectById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
-		UserBean sb = springTableService.select(id);
-        return sb;
+    public Result<UserBean> selectById(@ApiParam(value="用户id", defaultValue="1") @RequestParam(value="id", defaultValue="1") int id) {
+		UserBean sb = userService.select(id);
+        return Result.success(sb);
     }
     
     /**
@@ -51,7 +52,7 @@ public class UserController {
 	 */
 	@ApiOperation(value = "增加对象", notes = "增加对象")
 	@RequestMapping(value = "/add", method=RequestMethod.GET)
-    public String addUser(@ApiParam(value="用户名称", defaultValue="") @RequestParam(value="name", defaultValue="") String name, @ApiParam(value="用户备注", defaultValue="") @RequestParam(value="info", defaultValue="") String info) {
+    public Result<String> addUser(@ApiParam(value="用户名称", defaultValue="") @RequestParam(value="name", defaultValue="") String name, @ApiParam(value="用户备注", defaultValue="") @RequestParam(value="info", defaultValue="") String info) {
 		logger.info("addUser===>>>");
 		logger.error("addUser===>>>");
     	String re = "fail";
@@ -60,11 +61,11 @@ public class UserController {
     		sBean.setName(name);
     		sBean.setInfo(info);
     		sBean.setPw(name);
-    		int i = springTableService.insert(sBean);
+    		int i = userService.insert(sBean);
     		if (i>0) {
     			re = "success";
 			}
 		}
-        return re;
+        return Result.success(re);
     }
 }
