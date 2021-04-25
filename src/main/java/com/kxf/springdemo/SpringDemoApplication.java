c5472851b56f77757e82000550f1fe5845132a06
kuangxuefeng <1024883177@qq.com>
2021-04-25 14:56:41 +0800

    获取服务ip和端口

--- src/main/java/com/kxf/springdemo/SpringDemoApplication.java
+++ src/main/java/com/kxf/springdemo/SpringDemoApplication.java
@@ -1,9 +1,13 @@
 package com.kxf.springdemo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,9 +16,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		Logger logger=LoggerFactory.getLogger(SpringDemoApplication.class);
+		Logger logger = LoggerFactory.getLogger(SpringDemoApplication.class);
 		logger.error("开始启动服务===>>>");
-		SpringApplication.run(SpringDemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
+		String serverPort = context.getEnvironment().getProperty("server.port");
+		String serverIp = getIp();
+		logger.error("服务启动完成===>>>started at " + serverIp + ":" + serverPort);
+	}
+
+	public static String getIp() {
+		String ipStr = "http://";
+		InetAddress address = null;
+		try {
+			address = InetAddress.getLocalHost();
+			ipStr = ipStr + address.getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return ipStr;
 	}
 
 }
